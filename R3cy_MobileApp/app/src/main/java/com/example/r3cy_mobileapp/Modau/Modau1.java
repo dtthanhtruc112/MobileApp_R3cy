7c34342c84dbeaa069129e305d82f68e625cb70a
nhuquynh184 <quynhdln21411@st.uel.edu.vn>
2024-03-25 21:33:30 +0700

    add Modau, Thugom

---
+++ R3cy_MobileApp/app/src/main/java/com/example/r3cy_mobileapp/Modau/Modau1.java
@@ -0,0 +1,16 @@
+package com.example.r3cy_mobileapp.Modau;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.r3cy_mobileapp.R;
+
+public class Modau1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modau1);
+    }
+}
