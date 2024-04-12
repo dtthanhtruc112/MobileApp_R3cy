1ad287d14a8a1fee1acd4e3aef5b437d70fa1aa2
nhuquynh184 <quynhdln21411@st.uel.edu.vn>
2024-04-12 22:31:57 +0700

    tao chuyển động banner

---
+++ R3cy_MobileApp/app/src/main/java/com/example/models/Banners.java
@@ -0,0 +1,17 @@
+package com.example.models;
+
+public class Banners {
+    int bannerId;
+
+    public Banners(int bannerId) {
+        this.bannerId = bannerId;
+    }
+
+    public int getBannerId() {
+        return bannerId;
+    }
+
+    public void setBannerId(int bannerId) {
+        this.bannerId = bannerId;
+    }
+}
